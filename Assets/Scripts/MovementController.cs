5072d2ce0550d62d4f93a00f6561699c00a64c08
Jack Yang <jackyangzzh@gmail.com>
2020-10-25 17:08:42 -0500

    New movement controller

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -6,24 +6,46 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class MovementController : MonoBehaviour
 {
+    public float speed = 1.0f;
+
     public List<XRController> controllers;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    public GameObject head = null;
 
     // Update is called once per frame
     void Update()
     {
-        foreach (XRController controller in controllers)
+
+        foreach (XRController xRController in controllers)
         {
-            if (controller.inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 positionVector))
+            Debug.Log(xRController.inputDevice.name);
+
+            if (xRController.inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis,out Vector2 positionVector))
             {
-                Debug.Log(positionVector);
+                if (positionVector.magnitude > 0.15f)
+                {
+                    Debug.Log(positionVector);
+                    Move(positionVector);
+                }
+                
             }
+
         }
 
+    }
+
+
+    private void Move(Vector2 positionVector)
+    {
+        // Apply the touch position to the head's forward Vector
+        Vector3 direction = new Vector3(positionVector.x, 0, positionVector.y);
+        Vector3 headRotation = new Vector3(0, head.transform.eulerAngles.y, 0);
+
+        // Rotate the input direction by the horizontal head rotation
+        direction = Quaternion.Euler(headRotation) * direction;
+
+        // Apply speed and move
+        Vector3 movement = direction * speed;
+        transform.position += (Vector3.ProjectOnPlane(Time.deltaTime * movement, Vector3.up));
     }
 }

62ecdb6064d8afdc00803865b0d83029da9ab0c4
Jack Yang <jackyangzzh@gmail.com>
2020-10-24 21:25:48 -0500

    Add Joystick input

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class MovementController : MonoBehaviour
+{
+    public List<XRController> controllers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (XRController controller in controllers)
+        {
+            if (controller.inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 positionVector))
+            {
+                Debug.Log(positionVector);
+            }
+        }
+
+    }
+}

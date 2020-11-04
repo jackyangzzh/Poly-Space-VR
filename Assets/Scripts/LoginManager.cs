22424b8a42fafaa168a7f1ad4adfc2546fe8e6ee
Jack Yang <jackyangzzh@gmail.com>
2020-11-03 19:14:18 -0600

    Add Photon Pun

---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class LoginManager : MonoBehaviourPunCallbacks
+{
+    #region Unity Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #endregion
+
+    #region Photon Callback Methods
+    public override void OnConnected()
+    {
+        Debug.Log("On connected");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master");
+    }
+
+    #endregion
+}

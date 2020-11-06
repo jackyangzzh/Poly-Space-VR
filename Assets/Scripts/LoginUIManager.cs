d69c5e890fe41854083d2fed311cc51f7bfdcc2f
Jack Yang <jackyangzzh@gmail.com>
2020-11-05 18:47:55 -0600

    Login UI Manager

---
+++ Assets/Scripts/LoginUIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginUIManager : MonoBehaviour
+{
+    public GameObject ConnectOptionsPanel;
+    public GameObject ConnectWithNamePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectOptionsPanel.SetActive(true);
+        ConnectWithNamePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

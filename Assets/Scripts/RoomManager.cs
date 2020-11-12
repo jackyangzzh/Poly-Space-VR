249a55079a51542700d613c31f1d06335bf6617b
Jack Yang <jackyangzzh@gmail.com>
2020-11-11 20:05:45 -0600

    Connect to random room in progress

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #region UI Callback Methods
+
+    public void JoinRandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    #endregion
+
+    #region Photon Callback Methods
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log(message);
+    }
+
+    #endregion
+}

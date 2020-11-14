4caf7a66b1287db4fcc3fe380277bbf03641d39a
Jack Yang <jackyangzzh@gmail.com>
2020-11-13 18:54:23 -0600

    Login manager editor scripting

---
+++ Assets/Scripts/Editor/LoginManagerEditorScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LoginManager))]
+public class LoginManagerEditorScript : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.HelpBox("Responsible for connet to Photon Server", MessageType.Info);
+
+        LoginManager loginManager = (LoginManager)target;
+        if(GUILayout.Button("Connect Annoymously"))
+        {
+            loginManager.ConnectToPhoton();
+        }
+    }
+}
+

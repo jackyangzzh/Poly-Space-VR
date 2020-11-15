bfd7af37d840a10cd2364d7a10b552df35da9fc1
Jack Yang <jackyangzzh@gmail.com>
2020-11-15 15:16:31 -0600

    Add open world menu prefab

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     public GameObject menu;
+    public GameObject UIOpenWorld;
 
     // Start is called before the first frame update
     void Start()
     {
         menu.SetActive(false);
+        UIOpenWorld.SetActive(false);
     }
 
     public void OnWorldsButtonClicked()
     {
         Debug.Log("World button");
+        if (UIOpenWorld != null)
+        {
+            UIOpenWorld.SetActive(true);
+        }
     }
 
     public void OnHomeButtonClicked()

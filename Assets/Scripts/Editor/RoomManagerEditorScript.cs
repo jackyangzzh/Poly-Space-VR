566fc7003eae10e65e70f9b898efd49ca651d0c3
Jack Yang <jackyangzzh@gmail.com>
2020-11-14 19:07:32 -0600

    Room Editor scripting

---
+++ Assets/Scripts/Editor/RoomManagerEditorScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RoomManager))]
+public class RoomManagerEditorScript : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.HelpBox("Responsible for creating and joining rooms", MessageType.Info);
+
+        RoomManager roomManager = (RoomManager)target;
+        if (GUILayout.Button("Join Random Room"))
+        {
+            roomManager.JoinRandomRoom();
+        }
+    }
+}

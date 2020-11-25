b196d3446c5f05a8aa29a899588d6b650ee470d2
Jack Yang <jackyangzzh@gmail.com>
2020-11-24 18:40:10 -0600

    VR player prefab

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] GameObject genericVRPlayer;
+
+    public Vector3 SpawnPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.IsConnectedAndReady)
+        {
+            PhotonNetwork.Instantiate(genericVRPlayer.name, SpawnPosition, Quaternion.identity);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

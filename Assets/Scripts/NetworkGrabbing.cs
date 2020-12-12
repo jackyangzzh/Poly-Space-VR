957148b49e270f53303cfead73014150589eb6bf
Jack Yang <jackyangzzh@gmail.com>
2020-12-11 20:44:55 -0600

    Ownership transfer

---
+++ Assets/Scripts/NetworkGrabbing.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbacks
+{
+    PhotonView _photonView;
+
+    private void Awake()
+    {
+        _photonView = GetComponent<PhotonView>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnSelectEnter()
+    {
+        TransferOwnership();
+    }
+
+    public void OnSelectExit()
+    {
+
+    }
+
+    private void TransferOwnership()
+    {
+        _photonView.RequestOwnership();
+    }
+
+    public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
+    {
+        _photonView.TransferOwnership(requestingPlayer);
+    }
+
+    public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
+    {
+        
+    }
+}
